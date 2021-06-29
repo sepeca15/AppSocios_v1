04820714247685150727e3d44831afe0d8e51e2f
sepeca15 <sepeca15@gmail.com>
2021-06-29 18:26:53 -0300

    funcionalidad de modal en view infoPageEmpleado

--- src/components/AddInfoEmpleado.js
+++ src/components/AddInfoEmpleado.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from "prop-types";
 import * as Icon from 'react-feather';
+import swal from 'sweetalert';
 
 const AddInfoEmpleado = (props) => {
     return (
         <>
           <div className="bg-gray-200 md:w-full inline-block md:flex justify-between px-2 py-1 itemRow md:px-10 flex-col md:flex-row shadow-md">
                         <div className="flex my-3 md:my-0 lg:w-1/12 justify-around items-center">
-                            <img className="relative z-30 inline object-cover w-12 h-12 border-2 border-white rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image" />
+                            <img className=" inline object-cover w-12 h-12 border-2 border-white rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image" />
                             <label>Tata</label>
                         </div> 
                         <div className="flex my-2 md:my-0 lg:w-1/12 justify-start md:justify-center items-center">
@@ -34,8 +35,8 @@ const AddInfoEmpleado = (props) => {
                                 <Icon.Delete className="ml-2"/>
                                 Eliminar 
                             </button>
-                            <button className="bg-green1 text-xs ml-1 text-white font-bold p-2 rounded"> 
-                                <Icon.Edit className="ml-1" /> Editar
+                            <button className="bg-green1 text-xs ml-1 text-white font-bold p-2 rounded" onClick={props.openModal}> 
+                                <Icon.Edit className="ml-1"/> Editar
                             </button>
                         </div>
                     </div>
@@ -54,5 +55,6 @@ AddInfoEmpleado.propTypes = {
 	telefono: PropTypes.string,
 	ci: PropTypes.string,
 	cargo: PropTypes.string,
-	estado: PropTypes.bool
+	estado: PropTypes.bool,
+    openModal: PropTypes.func
 };
