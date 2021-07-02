408eee7f2151d8cff6669fdd724369da55caec89
sepeca15 <sepeca15@gmail.com>
2021-07-02 11:54:51 -0300

    actualizacion ok con todo lo de martin pero ahora si ok

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,18 +1,12 @@
 import { combineReducers } from 'redux'
 import { authReducer } from './reducers/authReducer'
-<<<<<<< HEAD
 import { empresasReducer } from './reducers/empresasReducer'
-
-export const rootReducer = combineReducers({
-    auth: authReducer,
-    empresas: empresasReducer
-
-})
-=======
 import { empleadoempresaReducer } from './reducers/empleadoempresaReducer'
 
 export const rootReducer = combineReducers({
     auth: authReducer,
+    empresas: empresasReducer,
     empleadosEmpresa: empleadoempresaReducer
+
 })
->>>>>>> 509fb66def2058b9caae83ad70e3b9ce4c515695
+
